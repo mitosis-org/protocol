1ab52223e2bfc886b915c8512b3cf1958840247e
byeongsu-hong <hong@byeongsu.dev>
2024-08-02 20:22:58 +0900

    init

---
+++ script/Multichain.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Vm} from 'forge-std/Vm.sol';
+import {Script} from 'forge-std/Script.sol';
+import {console} from 'forge-std/console.sol';
+
+import {LibString} from 'solady/utils/LibString.sol';
+
+/// @notice A example script to fetch multichain balances for the caller.
+/// @dev The caller can be set via using the flag like `account` or `private-key` or `mnemonic` etc...
+contract Multichain is Script {
+  using LibString for string;
+
+  function run() public {
+    Vm.Rpc[] memory rpcs = vm.rpcUrlStructs();
+
+    vm.startBroadcast();
+    (, address caller, ) = vm.readCallers();
+    vm.stopBroadcast();
+
+    for (uint256 i = 0; i < rpcs.length; i++) {
+      if (rpcs[i].url.eq('')) {
+        console.log('rpc url is empty. skipping. (key: %s)', rpcs[i].key);
+        continue;
+      }
+
+      vm.createSelectFork(rpcs[i].key);
+      uint256 balance = caller.balance;
+
+      console.log('caller balance for network %s: %d', rpcs[i].key, balance);
+    }
+  }
+}
