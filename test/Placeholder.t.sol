700e7b197cadd6a5f370ce1099ad383159b629db
ByeongSu Hong <hong@byeongsu.dev>
2024-08-05 15:18:08 +0900

    ✨ Etherscan support (#3)
    * add etherscan keys
    
    * use forge fmt
    
    * rollback to forge fmt
    
    * add codeowners
    
    * blockscout
    
    * remove api key from blockscout chains
    
    * use self-hosted runner
    
    * add KEY postfix

--- test/Placeholder.t.sol
+++ test/Placeholder.t.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {Vm} from 'forge-std/Vm.sol';
-import {Test} from 'forge-std/Test.sol';
-import {console} from 'forge-std/console.sol';
+import { Vm } from 'forge-std/Vm.sol';
+import { Test } from 'forge-std/Test.sol';
+import { console } from 'forge-std/console.sol';
 
-import {Placeholder} from '../src/Placeholder.sol';
+import { Placeholder } from '../src/Placeholder.sol';
 
 contract TestPlaceholder is Test {
   Placeholder internal placeholder;
