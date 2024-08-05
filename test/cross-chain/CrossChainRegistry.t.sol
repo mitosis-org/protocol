fb0c297dbcc5f774e35cd4e3ccc864f6ce1825ac
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-05 17:03:53 +0900

    test/cross-chain: add TestCrossChainRegistry

---
+++ test/cross-chain/CrossChainRegistry.t.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { Vm } from 'forge-std/Vm.sol';
+import { Test } from 'forge-std/Test.sol';
+import { console } from 'forge-std/console.sol';
+
+import { Storage } from '../../src/lib/Storage.sol';
+import { CrossChainRegistry } from '../../src/cross-chain/CrossChainRegistry.sol';
+import { MsgType } from '../../src/cross-chain/messages/Message.sol';
+
+contract TestCrossChainRegistry is Test {
+  CrossChainRegistry internal ccRegistry;
+
+  function setUp() public {
+    Storage strg = new Storage();
+    ccRegistry = new CrossChainRegistry(address(strg));
+  }
+
+  function test_setChain() public {
+    uint256 chainID = 1;
+    string memory name = 'Ethereum';
+    uint32 hplDomain = 1;
+
+    ccRegistry.setChain(chainID, name, hplDomain);
+
+    vm.expectRevert(); // 'already registered chain'
+    ccRegistry.setChain(chainID, name, hplDomain);
+
+    uint256[] memory chains = ccRegistry.getChains();
+    require(chains.length == 1, 'invalid getChains');
+    require(chains[0] == chainID, 'invalid getChains');
+    require(
+      keccak256(abi.encodePacked(ccRegistry.getChainName(1))) == keccak256(abi.encodePacked(name)),
+      'invalid getChainNmae'
+    );
+    require(ccRegistry.getHyperlaneDomain(chainID) == hplDomain, 'invalid getHyperlaneDomain');
+    require(ccRegistry.getChainByHyperlaneDomain(hplDomain) == chainID, 'invalid getChainByHyperlaneDomain');
+  }
+
+  function test_setVault() public {
+    uint256 chainID = 1;
+    address vault = address(1);
+    address underlyingAsset = address(2);
+
+    vm.expectRevert(); // 'not registered chain'
+    ccRegistry.setVault(chainID, vault, underlyingAsset);
+
+    string memory name = 'Ethereum';
+    uint32 hplDomain = 1;
+    ccRegistry.setChain(chainID, name, hplDomain);
+
+    ccRegistry.setVault(chainID, vault, underlyingAsset);
+    require(ccRegistry.getVault(chainID, underlyingAsset) == vault, 'invalid getVault');
+    require(ccRegistry.getVaultUnderlyingAsset(chainID, vault) == underlyingAsset, 'invalid getVault');
+  }
+
+  function test_setStrategyExecutor() public {
+    uint256 chainID = 1;
+    address eolVault = address(1);
+    uint256 strategyExecutorID = 1;
+
+    address ethereumStrategyExecutor = address(2);
+
+    vm.expectRevert(); // 'not registered chain'
+    ccRegistry.setStrategyExecutor(eolVault, strategyExecutorID, chainID, ethereumStrategyExecutor);
+
+    string memory name = 'Ethereum';
+    uint32 hplDomain = 1;
+    ccRegistry.setChain(chainID, name, hplDomain);
+
+    ccRegistry.setStrategyExecutor(eolVault, strategyExecutorID, chainID, ethereumStrategyExecutor);
+    require(
+      ccRegistry.getStrategyExecutor(eolVault, strategyExecutorID, chainID) == ethereumStrategyExecutor,
+      'invalid getStrategyExecutor'
+    );
+  }
+
+  function test_setHyperlaneRoute() public {
+    uint256 chainID = 1;
+    address msgDepositDestination = address(1);
+
+    vm.expectRevert(); // 'not registered chain'
+    ccRegistry.setHyperlaneRoute(chainID, MsgType.MsgDeposit, msgDepositDestination);
+
+    string memory name = 'Ethereum';
+    uint32 hplDomain = 1;
+    ccRegistry.setChain(chainID, name, hplDomain);
+
+    ccRegistry.setHyperlaneRoute(chainID, MsgType.MsgDeposit, msgDepositDestination);
+    require(
+      ccRegistry.getHyperlaneRoute(chainID, MsgType.MsgDeposit) == msgDepositDestination, 'invalid getHyperlaneRoute'
+    );
+  }
+}
