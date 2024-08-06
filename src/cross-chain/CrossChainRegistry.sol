47e5a4325ef8cf83585c703e32585f025b1be5b2
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-06 13:43:11 +0900

    src/cross-chain: delete RegistryBase

--- src/cross-chain/CrossChainRegistry.sol
+++ src/cross-chain/CrossChainRegistry.sol
@@ -1,24 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import { AccessControl } from '@oz-v5/access/AccessControl.sol';
-import { RegistryBase } from './RegistryBase.sol';
+import { AccessControlUpgradeable } from '@ozu-v5/access/AccessControlUpgradeable.sol';
+import { OwnableUpgradeable } from '@ozu-v5/access/OwnableUpgradeable.sol';
+import { Ownable2StepUpgradeable } from '@ozu-v5/access/Ownable2StepUpgradeable.sol';
 import { KVContainer } from '../lib/KVContainer.sol';
 import { Error } from '../lib/Error.sol';
 import { MsgType } from './messages/Message.sol';
 
-contract CrossChainRegistry is RegistryBase, AccessControl {
+contract CrossChainRegistryStorageV1 {
+  struct StorageV1 {
+    KVContainer kvContainer;
+    uint256[] chains;
+  }
+
+  bytes32 public constant WRITER_ROLE = keccak256('WRITER_ROLE');
+
+  // keccak256(abi.encode(uint256(keccak256("mitosis-protocol.storage.CrossChainRegistryStorageV1.v1")) - 1)) & ~bytes32(uint256(0xff))
+  bytes32 public constant StorageV1Location = 0x5df85134e0cd615622b98ff98ced84d19d412f4c64e5a4bd5c374ebc77026200;
+
+  function _getStorageV1() internal pure returns (StorageV1 storage $) {
+    // slither-disable-next-line assembly
+    assembly {
+      $.slot := StorageV1Location
+    }
+  }
+}
+
+contract CrossChainRegistry is Ownable2StepUpgradeable, AccessControlUpgradeable, CrossChainRegistryStorageV1 {
   event ChainSet(uint256 indexed chain, uint32 indexed hplDomain, string name);
   event VaultSet(uint256 indexed chain, address indexed vault, address indexed underlyingAsset);
   event HyperlaneRouteSet(uint32 indexed hplDomain, bytes1 indexed msgType, address indexed dest);
 
-  uint256[] _chains;
-
-  bytes32 public constant WRITER_ROLE = keccak256('WRITER_ROLE');
-
-  constructor(address containerAddress) {
+  constructor(address kvContainer) initializer {
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    _kvContainer = KVContainer(containerAddress);
+    _getStorageV1().kvContainer = KVContainer(kvContainer);
+  }
+
+  //=========== merge OwnableUpgradeable & Ownable2StepUpgradeable
+
+  function transferOwnership(address owner) public override {
+    Ownable2StepUpgradeable.transferOwnership(owner);
+  }
+
+  function _transferOwnership(address owner) internal override {
+    Ownable2StepUpgradeable._transferOwnership(owner);
+  }
+
+  //===========
+
+  function initialize(address owner) public initializer {
+    __Ownable2Step_init();
+    _transferOwnership(owner);
   }
 
   modifier onlyAdmin() {
@@ -41,31 +74,37 @@ contract CrossChainRegistry is RegistryBase, AccessControl {
   // View functions
 
   function getChains() public view returns (uint256[] memory) {
-    return _chains;
+    return _getStorageV1().chains;
   }
 
   function getChainName(uint256 chain) public view returns (string memory) {
-    return _getString(_getChainNameKey(chain));
+    bytes32 key = _getChainNameKey(chain);
+    return _getStorageV1().kvContainer.getString(key);
   }
 
   function getHyperlaneDomain(uint256 chain) public view returns (uint32) {
-    return uint32(_getUint(_getHyperlaneDomainKey(chain)));
+    bytes32 key = _getHyperlaneDomainKey(chain);
+    return uint32(_getStorageV1().kvContainer.getUint(key));
   }
 
   function getVault(uint256 chain, address asset) public view returns (address) {
-    return _getAddress(_getVaultKey(chain, asset));
+    bytes32 key = _getVaultKey(chain, asset);
+    return _getStorageV1().kvContainer.getAddress(key);
   }
 
   function getVaultUnderlyingAsset(uint256 chain, address vault) public view returns (address) {
-    return _getAddress(_getVaultUnderlyingAssetKey(chain, vault));
+    bytes32 key = _getVaultUnderlyingAssetKey(chain, vault);
+    return _getStorageV1().kvContainer.getAddress(key);
   }
 
   function getChainByHyperlaneDomain(uint32 hplDomain) public view returns (uint256) {
-    return _getUint(_getChainByHyperlaneDomainKey(hplDomain));
+    bytes32 key = _getChainByHyperlaneDomainKey(hplDomain);
+    return _getStorageV1().kvContainer.getUint(key);
   }
 
   function getHyperlaneRoute(uint32 hplDomain, MsgType msgType) public view returns (address) {
-    return _getAddress(_getHyperlaneRouteKey(hplDomain, msgType));
+    bytes32 key = _getHyperlaneRouteKey(hplDomain, msgType);
+    return _getStorageV1().kvContainer.getAddress(key);
   }
 
   // Mutative functions
@@ -82,27 +121,37 @@ contract CrossChainRegistry is RegistryBase, AccessControl {
     grantRole(WRITER_ROLE, target);
   }
 
+  function revokeWriter(address target) public onlyAdmin {
+    // AccessControl emit event `RoleRevoked`
+    revokeRole(WRITER_ROLE, target);
+  }
+
   function setChain(uint256 chain, string calldata name, uint32 hplDomain) public onlyWriter {
     if (_checkChainAlreadyRegistered(chain)) {
       revert Error.AlreadyRegistered();
     }
-    _chains.push(chain);
-    _setString(_getChainNameKey(chain), name);
-    _setUint(_getHyperlaneDomainKey(chain), hplDomain);
-    _setUint(_getChainByHyperlaneDomainKey(hplDomain), chain);
+    StorageV1 storage $ = _getStorageV1();
+    $.chains.push(chain);
+
+    KVContainer container = $.kvContainer;
+    container.setString(_getChainNameKey(chain), name);
+    container.setUint(_getHyperlaneDomainKey(chain), hplDomain);
+    container.setUint(_getChainByHyperlaneDomainKey(hplDomain), chain);
     emit ChainSet(chain, hplDomain, name);
   }
 
   function setVault(uint256 chain, address vault, address underlyingAsset) public onlyWriter onlyRegisteredChain(chain) {
-    _setAddress(_getVaultKey(chain, underlyingAsset), vault);
-    _setAddress(_getVaultUnderlyingAssetKey(chain, vault), underlyingAsset);
+    KVContainer container = _getStorageV1().kvContainer;
+    container.setAddress(_getVaultKey(chain, underlyingAsset), vault);
+    container.setAddress(_getVaultUnderlyingAssetKey(chain, vault), underlyingAsset);
     emit VaultSet(chain, vault, underlyingAsset);
   }
 
   function setHyperlaneRoute(uint32 hplDomain, MsgType msgType, address dest) external onlyWriter {
     uint256 chain = getChainByHyperlaneDomain(hplDomain);
     if (chain == 0) revert Error.NotRegistered();
-    _setAddress(_getHyperlaneRouteKey(hplDomain, msgType), dest);
+    KVContainer container = _getStorageV1().kvContainer;
+    container.setAddress(_getHyperlaneRouteKey(hplDomain, msgType), dest);
     emit HyperlaneRouteSet(hplDomain, bytes1(uint8(msgType)), dest);
   }
 
@@ -138,7 +187,7 @@ contract CrossChainRegistry is RegistryBase, AccessControl {
   }
 
   function _checkChainAlreadyRegistered(uint256 chain) internal view returns (bool) {
-    uint256[] memory chains = _chains;
+    uint256[] memory chains = _getStorageV1().chains;
     for (uint256 i = 0; i < chains.length; i++) {
       if (chain == chains[i]) {
         return true;
