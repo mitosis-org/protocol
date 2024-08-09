3e72f5d77ee9d215397c116e176a81c81f111382
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-09 22:37:02 +0900

    forge fmt

--- src/hub/cross-chain/CrossChainRegistryStorageV1.sol
+++ src/hub/cross-chain/CrossChainRegistryStorageV1.sol
@@ -5,6 +5,7 @@ import { ERC7201Utils } from '../../lib/ERC7201Utils.sol';
 
 contract CrossChainRegistryStorageV1 {
   using ERC7201Utils for string;
+
   struct ChainInfo {
     string name;
     uint32 hplDomain;
