0ef56d5b2d81c082380e3478373a18bb9095f385
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-12 18:37:04 +0900

    src/hub: add MitosisLedger

---
+++ src/hub/MitosisLedger.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { IMitosisLedger } from '../interfaces/hub/IMitosisLedger.sol';
+
+/// @dev: This contract only stores state related to balances.
+contract MitosisLedeger is IMitosisLedger {
+}
