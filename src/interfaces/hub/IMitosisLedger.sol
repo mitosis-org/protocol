e22fd7bbc0a890fc813a7a1aa4aa8604f75455ae
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-12 21:52:18 +0900

    fix CI

--- src/interfaces/hub/IMitosisLedger.sol
+++ src/interfaces/hub/IMitosisLedger.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.26;
 
 interface IMitosisLedger {
   function getAssetAmount(uint256 chainId, address asset) external view returns (uint256);
-  function getEolAllocateAmount(address miAsset) external view returns (uint256);
+  function getEOLAllocateAmount(address miAsset) external view returns (uint256);
 
   function recordDeposit(uint256 chainId, address asset, uint256 amount) external;
   function recordWithdraw(uint256 chainId, address asset, uint256 amount) external;
 
   function recordOptIn(address miAsset, uint256 amount) external;
   function recordOptOutRequest(address miAsset, uint256 amount) external;
-  function recordOptOutRelease(address miAsset, uint256 amount) external;
+  function recordDeallocateEOL(address miAsset, uint256 amount) external;
   function recordOptOutResolve(address miAsset, uint256 amount) external;
   function recordOptOutClaim(address miAsset, uint256 amount) external;
 }
--- src/interfaces/hub/cross-chain/ICrossChainRegistry.sol
+++ src/interfaces/hub/cross-chain/ICrossChainRegistry.sol
@@ -19,18 +19,9 @@ interface ICrossChainRegistry {
   /// @dev Returns the ChainID by Hyperlane domain.
   function getChainIdByHyperlaneDomain(uint32 hplDomain) external view returns (uint256);
 
-  /// @dev Returns true if the underlying asset is supported for the given ChainID.
-  function isSupportUnderlyingAsset(uint256 chainId, address underlyingAsset) external view returns (bool);
-
   /// @dev Sets the chain information including ChainID, name, and Hyperlane domain.
   function setChain(uint256 chainId, string calldata name, uint32 hplDomain) external;
 
   /// @dev Sets the vault address for a specific ChainID.
   function setVault(uint256 chainId, address vault) external;
-
-  /// @dev Sets the underlying asset for the contract.
-  function setUnderlyingAsset(address underlyingAsset) external;
-
-  /// @dev Sets the underlying asset for a specific ChainID.
-  function setUnderlyingAsset(uint256 chainId, address underlyingAsset) external;
 }
