d72898529d519ebcb25281c5785f3e354abf2046
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-12 18:42:06 +0900

    src/interfaces/hub: typo

--- src/interfaces/hub/IMitosisLedger.sol
+++ src/interfaces/hub/IMitosisLedger.sol
@@ -15,10 +15,10 @@ interface IMitosisLedger {
     function recordDeposit(uint256 chainId, address asset, uint256 amount) external;
     function recordWithdraw(uint256 chainId, address asset, uint256 amount) external;
     function recordOptIn(address miAsset, uint256 amount) external;
-    function recordOutputRequest(address miAsset, uint256 amount) external;
+    function recordOptOutRequest(address miAsset, uint256 amount) external;
     function recordOptOutRelease(address miAsset, uint256 amount) external;
-    function recordOutputResolve(address miAsset, uint256 amount) external;
-    function recordOutputClaim(address miAsset, uint256 amount) external;
+    function recordOptOutResolve(address miAsset, uint256 amount) external;
+    function recordOptOutClaim(address miAsset, uint256 amount) external;
 
     // TODO: EOL State get/set methods (finalized, resolved, released, pending)
 }
