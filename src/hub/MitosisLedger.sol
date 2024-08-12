7b20ff7b2a7e0a4ad6671e1c78d590eda51857dc
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-12 20:23:04 +0900

    src/hub: add some comments to MitosisLedger

--- src/hub/MitosisLedger.sol
+++ src/hub/MitosisLedger.sol
@@ -16,9 +16,29 @@ contract MitosisLedeger is IMitosisLedger {
   }
 
   struct EolState {
+    /// @dev pending is a temporary value reflecting the opt-out request
+    /// This value is used to calculate the EOL allocation for each chain.
+    /// (i.e. This means that from the point of the opt-out request, yield
+    /// will not be provided for the corresponding miAsset)
     uint256 pending;
+
+    /// @dev released is the state temporarily storing the value released
+    /// from the MitosisVault of the branch when it is uploaded. This value
+    /// is used when resolving the opt-out request.
+    /// Note: It is a state that allows the logic for settling in Mitosis L1
+    /// and the logic for resolving the opt-out request to be executed
+    /// separately.
     uint256 released;
+
+    /// @dev resvoled is the actual amount used for the opt-out request
+    /// resolvation.
+    //
+    // FIXME(ray)
     uint256 resolved;
+
+    /// @dev finalized is the total amount of assets that have been finalized
+    /// opt-in to Mitosis L1.
+    /// Note: This value must match the totalSupply of the miAsset.
     uint256 finalized;
   }
 
