08ffe93e0281f97e0256c8134c2a46363a6245b8
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-12 18:34:33 +0900

    src/interfaces/hub: fix IMitosisLedger

--- src/interfaces/hub/IMitosisLedger.sol
+++ src/interfaces/hub/IMitosisLedger.sol
@@ -2,18 +2,23 @@
 pragma solidity ^0.8.26;
 
 interface IMitosisLedger {
+    // View functions
+
     // Chain state
     function getAssetAmount(uint256 chainId, address asset) external view returns (uint256);
-    function getAssetAmountByAllChains(address asset) external view returns (uint256);
+
     // EOL state
     function getEolAllocateAmount(address miAsset) external view returns (uint256);
+
+    // Mutative functions
 
     function recordDeposit(uint256 chainId, address asset, uint256 amount) external;
     function recordWithdraw(uint256 chainId, address asset, uint256 amount) external;
     function recordOptIn(address miAsset, uint256 amount) external;
-    function recordOptOut(address miAsset, uint256 amount) external;
     function recordOutputRequest(address miAsset, uint256 amount) external;
+    function recordOptOutRelease(address miAsset, uint256 amount) external;
     function recordOutputResolve(address miAsset, uint256 amount) external;
+    function recordOutputClaim(address miAsset, uint256 amount) external;
 
     // TODO: EOL State get/set methods (finalized, resolved, released, pending)
 }
