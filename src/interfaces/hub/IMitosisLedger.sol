086df3aeab6effcf16568b8e662ed7647dabca7e
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-12 21:26:44 +0900

    delete meaningless comment

--- src/interfaces/hub/IMitosisLedger.sol
+++ src/interfaces/hub/IMitosisLedger.sol
@@ -2,23 +2,15 @@
 pragma solidity ^0.8.26;
 
 interface IMitosisLedger {
-    // View functions
-
-    // Chain state
     function getAssetAmount(uint256 chainId, address asset) external view returns (uint256);
-
-    // EOL state
     function getEolAllocateAmount(address miAsset) external view returns (uint256);
-
-    // Mutative functions
 
     function recordDeposit(uint256 chainId, address asset, uint256 amount) external;
     function recordWithdraw(uint256 chainId, address asset, uint256 amount) external;
+
     function recordOptIn(address miAsset, uint256 amount) external;
     function recordOptOutRequest(address miAsset, uint256 amount) external;
     function recordOptOutRelease(address miAsset, uint256 amount) external;
     function recordOptOutResolve(address miAsset, uint256 amount) external;
     function recordOptOutClaim(address miAsset, uint256 amount) external;
-
-    // TODO: EOL State get/set methods (finalized, resolved, released, pending)
 }
