1d743c3ae1ae6f05243b47505a5b8e1f4d338549
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-12 21:47:47 +0900

    forge fmt

--- src/interfaces/hub/IMitosisLedger.sol
+++ src/interfaces/hub/IMitosisLedger.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.26;
 
 interface IMitosisLedger {
-    function getAssetAmount(uint256 chainId, address asset) external view returns (uint256);
-    function getEolAllocateAmount(address miAsset) external view returns (uint256);
+  function getAssetAmount(uint256 chainId, address asset) external view returns (uint256);
+  function getEolAllocateAmount(address miAsset) external view returns (uint256);
 
-    function recordDeposit(uint256 chainId, address asset, uint256 amount) external;
-    function recordWithdraw(uint256 chainId, address asset, uint256 amount) external;
+  function recordDeposit(uint256 chainId, address asset, uint256 amount) external;
+  function recordWithdraw(uint256 chainId, address asset, uint256 amount) external;
 
-    function recordOptIn(address miAsset, uint256 amount) external;
-    function recordOptOutRequest(address miAsset, uint256 amount) external;
-    function recordOptOutRelease(address miAsset, uint256 amount) external;
-    function recordOptOutResolve(address miAsset, uint256 amount) external;
-    function recordOptOutClaim(address miAsset, uint256 amount) external;
+  function recordOptIn(address miAsset, uint256 amount) external;
+  function recordOptOutRequest(address miAsset, uint256 amount) external;
+  function recordOptOutRelease(address miAsset, uint256 amount) external;
+  function recordOptOutResolve(address miAsset, uint256 amount) external;
+  function recordOptOutClaim(address miAsset, uint256 amount) external;
 }
