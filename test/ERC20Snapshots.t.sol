c9e06e0c98b9ef66635f99e7bfc5308de3318f76
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-19 16:40:13 +0900

    add test codes

---
+++ test/ERC20Snapshots.t.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { Vm } from '@std/Vm.sol';
+import { Test } from '@std/Test.sol';
+import { console } from '@std/console.sol';
+
+import { ERC20Snapshots } from '../src/ERC20Snapshots.sol';
+
+contract TokenTest is Test {
+  ERC20Snapshots public token;
+
+  function setUp() public {
+    token = new ERC20Snapshots();
+    token.initialize('Token', 'TKN');
+  }
+
+  function test_AddSnapshots() public {
+    uint256 balance;
+    uint256 twab;
+    uint256 position;
+
+    (balance, twab,) = token.getLatestSnapshot(msg.sender);
+    assertEq(balance, 0);
+    assertEq(twab, 0);
+
+    vm.warp(block.timestamp + 1);
+    token.mint(msg.sender, 100 ether);
+
+    (balance, twab, position) = token.getLatestSnapshot(msg.sender);
+    assertEq(balance, 100 ether);
+    assertEq(twab, 0);
+
+    // // // // // // // // // // // // // //
+    // lastTwab + (lastBalance * duration) //
+    // // // // // // // // // // // // // //
+
+    vm.warp(block.timestamp + 100);
+    token.mint(msg.sender, 100 ether);
+
+    (balance, twab,) = token.getLatestSnapshot(msg.sender);
+    assertEq(balance, 200 ether);
+    // 0 + ( 100 ether * 100)
+    assertEq(twab, 100 ether * 100);
+
+    vm.warp(block.timestamp + 500);
+    token.mint(msg.sender, 100 ether);
+    (balance, twab, position) = token.getLatestSnapshot(msg.sender);
+    assertEq(balance, 300 ether);
+    assertEq(twab, (100 ether * 100) + (200 ether * 500));
+  }
+}
---
+++ test/lib/twab/Checkpoints.t.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { Vm } from '@std/Vm.sol';
+import { Test } from '@std/Test.sol';
+import { console } from '@std/console.sol';
+
+import '../../../src/lib/twab/Checkpoints.sol';
+
+contract CheckpointsTest is Test {
+  using Checkpoints for Checkpoints.Trace;
+
+  Checkpoints.Trace private trace;
+
+  function testPushAndLookup() public {
+    (uint208 balance, uint256 accumulatedTwab, uint48 position) = trace.latest();
+    assertEq(balance, 0);
+    assertEq(accumulatedTwab, 0);
+    trace.push(uint48(block.timestamp), 100, 1000);
+
+    (balance, accumulatedTwab, position) = trace.latest();
+    assertEq(balance, 100);
+    assertEq(accumulatedTwab, 1000);
+
+    trace.push(uint48(block.timestamp + 1), 200, 2000);
+
+    (balance, accumulatedTwab, position) = trace.latest();
+    assertEq(balance, 200);
+    assertEq(accumulatedTwab, 2000);
+
+    (balance, accumulatedTwab, position) = trace.lowerLookup(uint48(block.timestamp));
+    assertEq(balance, 100);
+    assertEq(accumulatedTwab, 1000);
+  }
+
+  function testLength() public {
+    assertEq(trace.length(), 0);
+    trace.push(uint48(block.timestamp), 100, 1000);
+    assertEq(trace.length(), 1);
+  }
+
+  function testLatestCheckpoint() public {
+    (bool exists, uint48 position, uint208 balance, uint256 accumulatedTwab) = trace.latestCheckpoint();
+    assertEq(exists, false);
+
+    trace.push(uint48(block.timestamp), 100, 1000);
+    (exists, position, balance, accumulatedTwab) = trace.latestCheckpoint();
+    assertEq(exists, true);
+    assertEq(balance, 100);
+    assertEq(accumulatedTwab, 1000);
+  }
+}
