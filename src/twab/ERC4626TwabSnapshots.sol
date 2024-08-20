e2e67c22d86ccd2386f3031a4b6dbdab5e4ba4e3
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-20 23:24:10 +0900

    header correction

--- src/twab/ERC4626TwabSnapshots.sol
+++ src/twab/ERC4626TwabSnapshots.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/extensions/ERC4626.sol)
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/extensions/ERC4626.sol)
 //
 // Modified for the Mitosis development.
 
