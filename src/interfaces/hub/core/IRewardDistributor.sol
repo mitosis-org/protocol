ee8939c86138da2027f043e4b505878a08d07961
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-27 17:06:27 +0900

    add methods to interface

--- src/interfaces/hub/core/IRewardDistributor.sol
+++ src/interfaces/hub/core/IRewardDistributor.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.8.26;
 
 interface IRewardDistributor {
   function dispatch(uint256 eolId, address asset, uint256 amount, uint48 rewardedAt) external;
+
+  function claim(uint256 eolId, address asset, uint48 rewardedAt) external;
+
+  function claim(uint256 eolId, address asset, uint256 amountsClaimed, uint48 rewardedAt, bytes32[] calldata proof)
+    external;
 }
--- src/interfaces/hub/core/IRewardTreasury.sol
+++ src/interfaces/hub/core/IRewardTreasury.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+import { IRewardDistributor } from './IRewardDistributor.sol';
+
 interface IRewardTreasury {
-  function deposit(address asset, uint256 amount, uint48 timestamp) external;
+  function deposit(uint256 eolId, address asset, uint256 amount, uint48 timestamp) external;
+
+  function dispatchTo(IRewardDistributor distributor, uint256 eolId, address asset, uint48 timestamp) external;
 }
