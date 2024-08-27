991c6c972a75327616e85fd084a3782591ce7c1d
Seungbae Yu <dbadoy4874@gmail.com>
2024-08-27 17:40:51 +0900

    introduce Batch, BatchStorage to RewardDistributor

--- src/hub/core/OnlyOwnerRewardDistributor.sol
+++ src/hub/core/OnlyOwnerRewardDistributor.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+import { Time } from '@oz-v5/utils/types/Time.sol';
 import { IERC20 } from '@oz-v5/interfaces/IERC20.sol';
 
 import { IRewardDistributor } from '../../interfaces/hub/core/IRewardDistributor.sol';
@@ -12,30 +13,60 @@ contract OnlyOwnerRewardDistributor is IRewardDistributor {
   address _owner;
   address _rewardTreasury;
 
-  mapping(uint256 eolId => mapping(address asset => uint256 amount)) _rewards;
+  mapping(uint256 eolId => mapping(address asset => BatchStorage batchStorage)) _batchStorages;
 
   constructor(address owner_, address rewardTreasury_) {
     _owner = owner_;
     _rewardTreasury = rewardTreasury_;
   }
 
+  function nextBatch(uint256 eolId, address asset) external returns (uint256) {
+    if (msg.sender != _owner) revert StdError.Unauthorized();
+    BatchStorage storage batchStorage = _batchStorages[eolId][asset];
+
+    if (batchStorage.currentBatchId > 0) {
+      batchStorage.batches[batchStorage.currentBatchId].endedAt = Time.timestamp();
+    }
+
+    uint256 nextBatchId = ++batchStorage.currentBatchId;
+    batchStorage.batches[nextBatchId].startedAt = Time.timestamp();
+
+    return nextBatchId;
+  }
+
   function dispatch(uint256 eolId, address asset, uint256 amount, uint48) external {
     if (msg.sender != _rewardTreasury) revert StdError.Unauthorized();
+
+    BatchStorage storage batchStorage = _batchStorages[eolId][asset];
+    if (batchStorage.currentBatchId == 0) revert('batchId not initialized');
+
     IERC20(asset).transferFrom(msg.sender, address(this), amount);
-    _rewards[eolId][asset] += amount;
+
+    batchStorage.batches[batchStorage.currentBatchId].totalReward += amount;
   }
 
-  function claim(uint256 eolId, address asset, uint48) external {
+  function claim(uint256 eolId, address asset, uint256 batchId) external {
+    claim(eolId, asset, batchId, _batchStorages[eolId][asset].batches[batchId].totalReward);
+  }
+
+  function claim(uint256 eolId, address asset, uint256 batchId, uint256 amountsClaimed) public {
     if (msg.sender != _owner) revert StdError.Unauthorized();
 
-    uint256 amount = _rewards[eolId][asset];
-    if (amount > 0) {
-      _rewards[eolId][asset] = 0;
-      IERC20(asset).transfer(_owner, amount);
-    }
+    BatchStorage storage batchStorage = _batchStorages[eolId][asset];
+    Batch storage batch = batchStorage.batches[batchId];
+
+    uint256 amount = batch.totalReward;
+    if (amount < amountsClaimed) revert('not enough');
+
+    batch.totalReward -= amountsClaimed;
+    IERC20(asset).transfer(_owner, amount);
   }
 
-  function claim(uint256, address, uint256, uint48, bytes32[] calldata) external pure {
+  function claim(uint256, address, uint256, bytes32[] calldata) external pure {
+    revert StdError.NotImplemented();
+  }
+
+  function claim(uint256, address, uint256, uint256, bytes32[] calldata) external pure {
     revert StdError.NotImplemented();
   }
 }
--- src/interfaces/hub/core/IRewardDistributor.sol
+++ src/interfaces/hub/core/IRewardDistributor.sol
@@ -2,10 +2,26 @@
 pragma solidity ^0.8.26;
 
 interface IRewardDistributor {
+  struct Batch {
+    uint256 totalReward;
+    uint48 startedAt;
+    uint48 endedAt;
+  }
+
+  // BatchStorage is assigned per eolId, per asset.
+  struct BatchStorage {
+    uint256 currentBatchId;
+    mapping(uint256 batchId => Batch batch) batches;
+  }
+
+  function nextBatch(uint256 eolId, address asset) external returns (uint256);
+
   function dispatch(uint256 eolId, address asset, uint256 amount, uint48 rewardedAt) external;
 
-  function claim(uint256 eolId, address asset, uint48 rewardedAt) external;
+  function claim(uint256 eolId, address asset, uint256 batchId) external;
+  function claim(uint256 eolId, address asset, uint256 batchId, uint256 amountsClaimed) external;
 
-  function claim(uint256 eolId, address asset, uint256 amountsClaimed, uint48 rewardedAt, bytes32[] calldata proof)
+  function claim(uint256 eolId, address asset, uint256 batchId, bytes32[] calldata proof) external;
+  function claim(uint256 eolId, address asset, uint256 batchId, uint256 amountsClaimed, bytes32[] calldata proof)
     external;
 }
