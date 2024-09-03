e27546fcdc2cfaff70ecd1ef534f172c4e683452
Seungbae Yu | Ray <dbadoy4874@gmail.com>
2024-09-03 18:01:57 +0900

    🐞 Fix CI (#41)
    * fix incorrect import
    
    * use `url` instead of `git` on `foundry.toml`.dependencies
    
    * add soldeer.toml
    
    * Update soldeer.lock
    
    * Update test.yml
    
    * test
    
    * `fix/ci` -> `main`

--- src/hub/eol/storage/EOLProtocolRegistryStorageV1.sol
+++ src/hub/eol/storage/EOLProtocolRegistryStorageV1.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.26;
 
 import { EnumerableSet } from '@oz-v5/utils/structs/EnumerableSet.sol';
 
-import { ProtocolInfo } from '../../interfaces/eol/IEOLProtocolRegistry.sol';
-import { ERC7201Utils } from '../../lib/ERC7201Utils.sol';
+import { ProtocolInfo } from '../../../interfaces/eol/IEOLProtocolRegistry.sol';
+import { ERC7201Utils } from '../../../lib/ERC7201Utils.sol';
 
 contract EOLProtocolRegistryStorageV1 {
   using ERC7201Utils for string;
