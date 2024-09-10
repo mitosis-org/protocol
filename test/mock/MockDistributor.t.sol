5d6ebe36d27f302046f4d0e4479fb2ebd697abe3
Seungbae Yu <dbadoy4874@gmail.com>
2024-09-10 14:16:40 +0900

    Update MockDistributor.t.sol

--- test/mock/MockDistributor.t.sol
+++ test/mock/MockDistributor.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
+import { IRewardConfigurator } from '../../src/interfaces/hub/reward/IRewardConfigurator.sol';
 import { IRewardDistributor, DistributionType } from '../../src/interfaces/hub/reward/IRewardDistributor.sol';
 
 contract MockDistributor is IRewardDistributor {
@@ -18,11 +19,25 @@ contract MockDistributor is IRewardDistributor {
     return 'MockDistributor';
   }
 
+  function rewardConfigurator() external pure returns (IRewardConfigurator) {
+    return IRewardConfigurator(address(0));
+  }
+
   function claimable(address, address, address, bytes memory) external pure returns (bool) {
     return true;
   }
 
+  function claimableAmount(address, address, address, bytes calldata) external view returns (uint256) {
+    return 0;
+  }
+
+  function claim(address eolVault, address reward, bytes calldata metadata) external { }
+
+  function claim(address eolVault, address reward, uint256 amount, bytes calldata metadata) external { }
+
   function handleReward(address, address, uint256, bytes memory) external pure {
     return;
   }
+
+  function setRewardManager(address rewardManager_) external { }
 }
