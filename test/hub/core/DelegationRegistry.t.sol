719f1aaaf5b0b3c55709eb2b247e13034f302b5f
Seungbae Yu | Ray <dbadoy4874@gmail.com>
2024-10-04 19:22:26 +0900

    👷‍♂️ Add `hub/core/DelegationRegistry` test code (#127)
    Create DelegationRegistry.t.sol

---
+++ test/hub/core/DelegationRegistry.t.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import { console } from '@std/console.sol';
+import { Test } from '@std/Test.sol';
+import { Vm } from '@std/Vm.sol';
+
+import { IERC20Errors } from '@oz-v5/interfaces/draft-IERC6093.sol';
+import { ProxyAdmin } from '@oz-v5/proxy/transparent/ProxyAdmin.sol';
+import { TransparentUpgradeableProxy } from '@oz-v5/proxy/transparent/TransparentUpgradeableProxy.sol';
+
+import { DelegationRegistry } from '../../../src/hub/core/DelegationRegistry.sol';
+
+contract MockRedistributionRule {}
+
+contract DelegationRegistryTest is Test {
+  DelegationRegistry internal _delegationRegistry;
+  ProxyAdmin internal _proxyAdmin;
+  MockRedistributionRule internal _redistributionRule;
+
+  address immutable owner = makeAddr('owner');
+  address immutable user1 = makeAddr('user1');
+  address immutable delegationManager = makeAddr('delegationManager');
+  address immutable defaultDelegatee = makeAddr('defaultDelegatee');
+  address immutable redistributionRule = makeAddr('redistributionRule');
+
+  function setUp() public {
+    _proxyAdmin = new ProxyAdmin(owner);
+
+    DelegationRegistry delgationRegistryImpl = new DelegationRegistry();
+    _delegationRegistry = DelegationRegistry(
+      payable(
+        address(
+          new TransparentUpgradeableProxy(
+            address(delgationRegistryImpl), address(_proxyAdmin), abi.encodeCall(_delegationRegistry.initialize, ())
+          )
+        )
+      )
+    );
+
+    _redistributionRule = new MockRedistributionRule();
+  }
+
+  function test_setDelegationManager() public {
+    assertEq(_delegationRegistry.delegationManager(user1), user1);
+
+    vm.prank(user1);
+    _delegationRegistry.setDelegationManager(user1, delegationManager);
+
+    assertEq(_delegationRegistry.delegationManager(user1), delegationManager);
+  }
+
+  function test_setDelegationManager_auth() public {
+    test_setDelegationManager();
+
+    vm.prank(delegationManager);
+    _delegationRegistry.setDelegationManager(user1, address(1));
+
+    assertEq(_delegationRegistry.delegationManager(user1), address(1));
+
+    vm.prank(user1);
+    _delegationRegistry.setDelegationManager(user1, address(3));
+
+    assertEq(_delegationRegistry.delegationManager(user1), address(3));
+  }
+
+  function test_setDefaultDelegatee() public {
+    assertEq(_delegationRegistry.defaultDelegatee(user1), address(0));
+
+    vm.prank(user1);
+    _delegationRegistry.setDefaultDelegatee(user1, defaultDelegatee);
+
+    assertEq(_delegationRegistry.defaultDelegatee(user1), defaultDelegatee);
+  }
+
+  function test_setDefaultDelegatee_auth() public {
+    vm.prank(user1);
+    _delegationRegistry.setDelegationManager(user1, delegationManager);
+    assertEq(_delegationRegistry.delegationManager(user1), delegationManager);
+
+    vm.prank(delegationManager);
+    _delegationRegistry.setDefaultDelegatee(user1, address(1));
+
+    assertEq(_delegationRegistry.defaultDelegatee(user1), address(1));
+
+    vm.prank(user1);
+    _delegationRegistry.setDefaultDelegatee(user1, address(3));
+
+    assertEq(_delegationRegistry.defaultDelegatee(user1), address(3)); 
+  }
+
+  function test_setRedistributionRule() public {
+    assertEq(_delegationRegistry.redistributionRule(user1), address(0));
+
+    vm.prank(user1);
+    _delegationRegistry.setRedistributionRule(user1, address(_redistributionRule));
+
+    assertEq(_delegationRegistry.redistributionRule(user1), address(_redistributionRule));
+  }
+
+  function test_setRedistributionRule_auth() public {
+    vm.prank(user1);
+    _delegationRegistry.setDelegationManager(user1, delegationManager);
+    assertEq(_delegationRegistry.delegationManager(user1), delegationManager);
+
+    MockRedistributionRule newRedistributionRule1 = new MockRedistributionRule();
+
+    vm.prank(delegationManager);
+    _delegationRegistry.setRedistributionRule(user1, address(newRedistributionRule1));
+
+    assertEq(_delegationRegistry.redistributionRule(user1), address(newRedistributionRule1));
+
+    MockRedistributionRule newRedistributionRule2 = new MockRedistributionRule();
+
+    vm.prank(user1);
+    _delegationRegistry.setRedistributionRule(user1, address(newRedistributionRule2));
+
+    assertEq(_delegationRegistry.redistributionRule(user1), address(newRedistributionRule2));
+  }
+}
