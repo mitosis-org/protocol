e392807801a7a3f7f7abd3c2c1e4f55c4e4606f9
Seungbae Yu | Ray <dbadoy4874@gmail.com>
2024-10-21 15:12:52 +0900

    🥢 Add a view method `tefi` to TeFiStrategy (#143)

--- src/branch/strategy/strategies/TeFi.sol
+++ src/branch/strategy/strategies/TeFi.sol
@@ -4,18 +4,9 @@ pragma solidity 0.8.27;
 import { IERC20 } from '@oz-v5/token/ERC20/IERC20.sol';
 import { SafeERC20 } from '@oz-v5/token/ERC20/utils/SafeERC20.sol';
 
+import { ITeFi } from '../../../interfaces/branch/strategy/ITeFi.sol';
 import { StdError } from '../../../lib/StdError.sol';
 import { StdStrategy } from './StdStrategy.sol';
-
-interface ITeFi {
-  error ITeFi__InsufficientDepositAmount();
-
-  function name() external view returns (string memory);
-  function asset() external view returns (IERC20);
-  function claimableAmount(address asset) external view returns (uint256);
-  function claim(address asset_, uint256 amount) external;
-  function withdraw(uint256 amount) external;
-}
 
 /**
  * @title TeFi
