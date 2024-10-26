413afcf2f52aa124b92d13841610685abdef521b
Seungbae Yu <dbadoy4874@gmail.com>
2024-10-26 12:22:10 +0900

    add view method `strategyId` to EOLStrategyExecutor

--- src/interfaces/branch/strategy/IEOLStrategyExecutor.sol
+++ src/interfaces/branch/strategy/IEOLStrategyExecutor.sol
@@ -41,11 +41,12 @@ interface IEOLStrategyExecutor {
 
   // storage v1
   function strategist() external view returns (address strategist_);
+  function strategyId(address implementation) external view returns (uint256);
 
-  function getStrategy(uint256 strategyId) external view returns (Strategy memory);
+  function getStrategy(uint256 strategyId_) external view returns (Strategy memory);
   function getStrategy(address implementation) external view returns (Strategy memory);
   function getEnabledStrategyIds() external view returns (uint256[] memory);
-  function isStrategyEnabled(uint256 strategyId) external view returns (bool);
+  function isStrategyEnabled(uint256 strategyId_) external view returns (bool);
   function isStrategyEnabled(address implementation) external view returns (bool);
 
   function totalBalance() external view returns (uint256);
--- test/mock/MockEOLStrategyExecutor.t.sol
+++ test/mock/MockEOLStrategyExecutor.t.sol
@@ -30,11 +30,12 @@ contract MockEOLStrategyExecutor is IEOLStrategyExecutor {
   }
 
   function strategist() external view returns (address) { }
+  function strategyId(address implementation) external view returns (uint256) {}
 
-  function getStrategy(uint256 strategyId) external view returns (Strategy memory) { }
+  function getStrategy(uint256 strategyId_) external view returns (Strategy memory) { }
   function getStrategy(address implementation) external view returns (Strategy memory) { }
   function getEnabledStrategyIds() external view returns (uint256[] memory) { }
-  function isStrategyEnabled(uint256 strategyId) external view returns (bool) { }
+  function isStrategyEnabled(uint256 strategyId_) external view returns (bool) { }
   function isStrategyEnabled(address implementation) external view returns (bool) { }
 
   function totalBalance() external view returns (uint256) {
