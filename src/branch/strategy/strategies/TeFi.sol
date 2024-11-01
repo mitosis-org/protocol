41e5910a9c24013e7b2064fb07e6c537828c8cb5
ByeongSu Hong <hong@byeongsu.dev>
2024-11-01 22:32:12 +0700

    ✨ Make TeFi, Tracle Upgradeable (#162)
    upgradeable

--- src/branch/strategy/strategies/TeFi.sol
+++ src/branch/strategy/strategies/TeFi.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
+import { Initializable } from '@oz-v5/proxy/utils/Initializable.sol';
 import { IERC20 } from '@oz-v5/token/ERC20/IERC20.sol';
 import { SafeERC20 } from '@oz-v5/token/ERC20/utils/SafeERC20.sol';
 
@@ -12,12 +13,12 @@ import { StdStrategy } from './StdStrategy.sol';
  * @title TeFi
  * @notice Simple DeFi contract for testnet
  */
-contract TeFi is ITeFi {
+contract TeFi is ITeFi, Initializable {
   using SafeERC20 for IERC20;
 
   IERC20 internal immutable _asset;
   address internal immutable _strategyExecutor;
-  string internal _name;
+  bytes32 internal immutable _name;
 
   modifier onlyStrategyExecutor() {
     require(msg.sender == _strategyExecutor, StdError.Unauthorized());
@@ -27,11 +28,18 @@ contract TeFi is ITeFi {
   constructor(address strategyExecutor_, address asset_, string memory name_) {
     _strategyExecutor = strategyExecutor_;
     _asset = IERC20(asset_);
-    _name = name_;
+    require(bytes(name_).length < 32, 'TeFi: name too long');
+    _name = bytes32(bytes(name_));
+
+    _disableInitializers();
+  }
+
+  function initialize() external initializer {
+    // to nothing
   }
 
   function name() external view returns (string memory) {
-    return _name;
+    return string(abi.encodePacked(_name));
   }
 
   function asset() external view returns (IERC20) {
