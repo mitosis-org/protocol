08e210d1daad190c41aed84a0e5277aa85c3536b
Seungbae Yu | Ray <dbadoy4874@gmail.com>
2024-11-03 17:34:57 +0700

    🐞 Add approve step before withdraw in OptOutQueue (#171)

--- src/hub/eol/OptOutQueue.sol
+++ src/hub/eol/OptOutQueue.sol
@@ -221,6 +221,7 @@ contract OptOutQueue is IOptOutQueue, Pausable, Ownable2StepUpgradeable, OptOutQ
   function _sync(StorageV1 storage $, IEOLVault eolVault, uint256 assets) internal {
     EOLVaultState storage eolVaultState = $.states[address(eolVault)];
 
+    eolVault.approve(address(eolVault), assets);
     eolVault.withdraw(assets, address(this), address(this));
 
     eolVaultState.queue.reserve(assets, eolVault.totalSupply(), eolVault.totalAssets(), eolVault.clock());
