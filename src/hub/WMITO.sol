a39c7a82e6d1f1e59069ab8e89780b94252250dc
byeongsu-hong <hong@byeongsu.dev>
2024-11-07 02:36:30 +0700

    metadata

--- src/hub/WMITO.sol
+++ src/hub/WMITO.sol
@@ -3,4 +3,14 @@ pragma solidity ^0.8.27;
 
 import { WETH } from '@solady/tokens/WETH.sol';
 
-contract WMITO is WETH { }
+contract WMITO is WETH {
+  /// @dev Returns the name of the token.
+  function name() public view virtual override returns (string memory) {
+    return 'Wrapped MITO';
+  }
+
+  /// @dev Returns the symbol of the token.
+  function symbol() public view virtual override returns (string memory) {
+    return 'WMITO';
+  }
+}
