f225363ad20fba313f35f2c84f28a59a5ca9c10f
byeongsu-hong <hong@byeongsu.dev>
2025-02-10 19:29:44 +0900

    solc

--- test/hub/core/HubAssetFactory.t.sol
+++ test/hub/core/HubAssetFactory.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.28;
 
 import { ERC1967Factory } from '@solady/utils/ERC1967Factory.sol';
 import { UpgradeableBeacon } from '@solady/utils/UpgradeableBeacon.sol';
--- test/hub/matrix/MatrixVaultFactory.t.sol
+++ test/hub/matrix/MatrixVaultFactory.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.28;
 
 import { WETH } from '@solady/tokens/WETH.sol';
 import { ERC1967Factory } from '@solady/utils/ERC1967Factory.sol';
