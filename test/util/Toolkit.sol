ebdd3da49ef473fc83959b03313a1c1653318979
byeongsu-hong <hong@byeongsu.dev>
2025-02-13 04:14:16 +0900

    add now

--- test/util/Toolkit.sol
+++ test/util/Toolkit.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 import { Test } from '@std/Test.sol';
 
 import { OwnableUpgradeable } from '@ozu-v5/access/OwnableUpgradeable.sol';
+import { SafeCast } from '@oz-v5/utils/math/SafeCast.sol';
 
 import { ERC1967Utils } from '@oz-v5/proxy/ERC1967/ERC1967Utils.sol';
 
@@ -11,6 +12,8 @@ import { Pausable } from '../../src/lib/Pausable.sol';
 import { StdError } from '../../src/lib/StdError.sol';
 
 contract Toolkit is Test {
+  using SafeCast for uint256;
+
   function _erc1967Impl(address target) internal view returns (address) {
     return address(uint160(uint256(vm.load(target, ERC1967Utils.IMPLEMENTATION_SLOT))));
   }
@@ -21,6 +24,14 @@ contract Toolkit is Test {
 
   function _erc1967Beacon(address target) internal view returns (address) {
     return address(uint160(uint256(vm.load(target, ERC1967Utils.BEACON_SLOT))));
+  }
+
+  function _now() internal view returns (uint256) {
+    return block.timestamp;
+  }
+
+  function _now48() internal view returns (uint48) {
+    return block.timestamp.toUint48();
   }
 
   // OwnableUpgradeable
