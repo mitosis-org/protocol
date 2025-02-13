d578c5b0494004e520253136b7068ef68317750f
byeongsu-hong <hong@byeongsu.dev>
2025-02-13 12:08:58 +0900

    test for happy cases

--- test/lib/LibRedeemQueue.t.sol
+++ test/lib/LibRedeemQueue.t.sol
@@ -19,15 +19,17 @@ contract LibRedeemQueueTest is Toolkit {
 
   function setUp() public { }
 
-  function test_enqueue() public {
+  function test_enqueue(uint256 amountPerEnqueue) public {
+    vm.assume(1 ether <= amountPerEnqueue && amountPerEnqueue < 1000 ether);
+
     for (uint256 i = 0; i < 3; i++) {
-      _enqueue(makeAddr('recipient-1'), 1 ether, bytes('A'));
-      _enqueue(makeAddr('recipient-2'), 1 ether, bytes('B'));
-      _enqueue(makeAddr('recipient-3'), 1 ether, bytes('C'));
+      _enqueue(makeAddr('recipient-1'), amountPerEnqueue, bytes('A'));
+      _enqueue(makeAddr('recipient-2'), amountPerEnqueue, bytes('B'));
+      _enqueue(makeAddr('recipient-3'), amountPerEnqueue, bytes('C'));
     }
 
-    assertEq(q.totalPendingAmount(), 9 ether);
-    assertEq(q.totalRequestAmount(), 9 ether);
+    assertEq(q.totalPendingAmount(), amountPerEnqueue * 9);
+    assertEq(q.totalRequestAmount(), amountPerEnqueue * 9);
     assertEq(q.totalReservedAmount(), 0);
 
     assertEq(q.size, 9);
@@ -36,11 +38,28 @@ contract LibRedeemQueueTest is Toolkit {
     assertEq(q.indexes[makeAddr('recipient-3')].size, 3);
   }
 
-  function test_reserve() public { }
+  function test_reserve(uint256 amountPerReserve) public {
+    vm.assume(1 ether <= amountPerReserve && amountPerReserve < 1000 ether);
 
-  function test_claim() public { }
+    _reserve(makeAddr('strategist-1'), amountPerReserve, bytes('A'));
+    _reserve(makeAddr('strategist-2'), amountPerReserve, bytes('B'));
+    _reserve(makeAddr('strategist-3'), amountPerReserve, bytes('C'));
+  }
 
-  function _enqueue(address recipient, uint256 amount, bytes memory metadata) private {
+  function test_claim(uint256 amountPerRequest) public {
+    vm.assume(1 ether <= amountPerRequest && amountPerRequest < 1000 ether);
+
+    test_enqueue(amountPerRequest);
+    test_reserve(amountPerRequest * 3);
+    vm.warp(block.timestamp + 1);
+
+    // now all of requests should be claimable, because redeemPeriod is zero
+    _assertClaimed(makeAddr('recipient-1'), amountPerRequest, amountPerRequest * 3, 3);
+    _assertClaimed(makeAddr('recipient-2'), amountPerRequest, amountPerRequest * 3, 3);
+    _assertClaimed(makeAddr('recipient-3'), amountPerRequest, amountPerRequest * 3, 3);
+  }
+
+  function _enqueue(address recipient, uint256 amount, bytes memory metadata) private returns (uint256) {
     uint48 createdAt = _now48();
     uint256 reqId = q.enqueue(recipient, amount, createdAt, metadata);
 
@@ -51,10 +70,37 @@ contract LibRedeemQueueTest is Toolkit {
     assertEq(metadata, q.data[reqId].metadata);
 
     assertEq(q.indexes[recipient].data[q.indexes[recipient].size - 1], reqId);
+
+    return reqId;
   }
 
-  function _reserve(address executor, uint256 amount, bytes memory metadata) private {
+  function _reserve(address executor, uint256 amount, bytes memory metadata) private returns (uint256) {
     uint48 reservedAt = _now48();
     uint256 logId = q.reserve(executor, amount, reservedAt, metadata);
+
+    assertEq(
+      amount, logId == 0 ? amount : q.reserveHistory[logId].accumulated - q.reserveHistory[logId - 1].accumulated
+    );
+    assertEq(reservedAt, q.reserveHistory[logId].reservedAt);
+    assertEq(metadata, q.reserveHistory[logId].metadata);
+
+    return logId;
+  }
+
+  function _claim(address recipient, uint256 maxClaimSize) private returns (uint256, uint256) {
+    uint256 prevOffset = q.indexes[recipient].offset;
+    uint256 totalClaimed = q.claim(recipient, maxClaimSize, _now48());
+    return (totalClaimed, q.indexes[recipient].offset - prevOffset);
+  }
+
+  function _assertClaimed(
+    address recipient,
+    uint256 maxClaimSize,
+    uint256 expectedTotalClaimed,
+    uint256 expectedClaimedCount
+  ) private {
+    (uint256 totalClaimed, uint256 claimedCount) = _claim(recipient, maxClaimSize);
+    assertEq(totalClaimed, expectedTotalClaimed);
+    assertEq(claimedCount, expectedClaimedCount);
   }
 }
