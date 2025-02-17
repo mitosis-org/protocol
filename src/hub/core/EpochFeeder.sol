c8dbc52b58ab6b526d97a3b1ad047871fdd0b5c9
byeongsu-hong <hong@byeongsu.dev>
2025-02-17 20:44:03 +0900

    audit EpochFeeder

--- src/hub/core/EpochFeeder.sol
+++ src/hub/core/EpochFeeder.sol
@@ -7,6 +7,11 @@ import { UUPSUpgradeable } from '@ozu-v5/proxy/utils/UUPSUpgradeable.sol';
 import { IEpochFeeder } from '../../interfaces/hub/core/IEpochFeeder.sol';
 import { StdError } from '../../lib/StdError.sol';
 
+/**
+ * @title EpochFeeder
+ * @notice Manages epoch transitions and timing for the protocol
+ * @dev This contract is upgradeable using UUPS pattern
+ */
 contract EpochFeeder is IEpochFeeder, Ownable2StepUpgradeable, UUPSUpgradeable {
   uint96 private _epoch;
   uint48 private _interval;
@@ -17,21 +22,30 @@ contract EpochFeeder is IEpochFeeder, Ownable2StepUpgradeable, UUPSUpgradeable {
     _disableInitializers();
   }
 
+  /// @inheritdoc IEpochFeeder
   function initialize(address owner_, uint48 interval_) external initializer {
+    require(interval_ == 0, IntervalTooShort());
+
     __Ownable_init(owner_);
     _epoch = 1;
     _interval = interval_;
+    _lastRoll = uint48(block.timestamp); // Set initial lastRoll
   }
 
+  /// @inheritdoc IEpochFeeder
   function clock() public view returns (uint48) {
     return uint48(block.timestamp - _lastRoll);
   }
 
+  /// @inheritdoc IEpochFeeder
   function epoch() public view returns (uint96) {
     return _epoch;
   }
 
+  /// @inheritdoc IEpochFeeder
   function epochAt(uint48 timestamp_) public view returns (uint96) {
+    require(timestamp_ != 0, InvalidEpoch());
+
     uint96 left = 1;
     uint96 right = _epoch;
 
@@ -53,29 +67,41 @@ contract EpochFeeder is IEpochFeeder, Ownable2StepUpgradeable, UUPSUpgradeable {
     return right;
   }
 
+  /// @inheritdoc IEpochFeeder
   function epochToTime(uint96 epoch_) public view returns (uint48) {
+    require(epoch_ == 0 || epoch_ > _epoch, InvalidEpoch());
     return _epochToTime[epoch_];
   }
 
+  /// @inheritdoc IEpochFeeder
   function interval() public view returns (uint48) {
     return _interval;
   }
 
+  /// @inheritdoc IEpochFeeder
   function lastRoll() public view returns (uint48) {
     return _lastRoll;
   }
 
+  /// @inheritdoc IEpochFeeder
   function roll() external onlyOwner {
-    uint48 now_ = clock();
+    uint48 now_ = uint48(block.timestamp);
     require(now_ > _lastRoll + _interval, StdError.Unauthorized());
 
     _epochToTime[_epoch] = now_;
-
     _lastRoll = now_;
     _epoch++;
+
+    emit EpochRolled(_epoch, now_);
   }
 
   // ================== UUPS ================== //
 
-  function _authorizeUpgrade(address newImplementation) internal override onlyOwner { }
+  /**
+   * @notice Authorizes an upgrade to a new implementation
+   * @param newImplementation Address of new implementation
+   */
+  function _authorizeUpgrade(address newImplementation) internal view override onlyOwner {
+    require(newImplementation != address(0), StdError.ZeroAddress('newImpl'));
+  }
 }
--- src/interfaces/hub/core/IEpochFeeder.sol
+++ src/interfaces/hub/core/IEpochFeeder.sol
@@ -1,12 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+/// @title IEpochFeeder
+/// @notice Interface for managing epoch transitions and timing in the protocol
 interface IEpochFeeder {
+  /// @notice Emitted when a new epoch is rolled
+  /// @param epoch The new epoch number
+  /// @param timestamp The timestamp when the epoch was rolled
+  event EpochRolled(uint96 indexed epoch, uint48 timestamp);
+
+  /// @dev Thrown when interval is set to zero
+  error IntervalTooShort();
+
+  /// @dev Thrown when epoch is invalid (zero or greater than current)
+  error InvalidEpoch();
+
+  /// @notice Returns time elapsed since last roll
+  /// @return timestamp Current clock time in seconds since last roll
   function clock() external view returns (uint48);
+
+  /// @notice Returns current epoch number, starting from 1
+  /// @return epochCurrent epoch number
   function epoch() external view returns (uint96);
+
+  /// @notice Finds epoch number at a given timestamp using binary search
+  /// @param timestamp_ Timestamp to query
+  /// @return epoch Epoch number at that timestamp
+  /// @dev Reverts if timestamp is 0
   function epochAt(uint48 timestamp_) external view returns (uint96);
+
+  /// @notice Gets timestamp for a specific epoch
+  /// @param epoch Epoch number to query
+  /// @return timestamp Timestamp when that epoch was created
+  /// @dev Reverts if epoch is 0 or greater than current epoch
   function epochToTime(uint96 epoch) external view returns (uint48);
+
+  /// @notice Returns interval between epochs
+  /// @return interval Current interval in seconds
   function interval() external view returns (uint48);
+
+  /// @notice Returns timestamp of last epoch roll
+  /// @return timestamp Timestamp of last roll
   function lastRoll() external view returns (uint48);
+
+  /// @notice Advances to next epoch if interval has passed
+  /// @dev Only callable by owner
+  /// @dev Reverts if insufficient time has passed since last roll
   function roll() external;
 }
