782b09db1b7716bbd9a7341f190a47f416742c87
byeongsu-hong <hong@byeongsu.dev>
2025-02-17 13:10:37 +0900

    factory

---
+++ src/branch/strategy/MatrixStrategyExecutorFactory.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.23 <0.9.0;
+
+import { BeaconProxy } from '@oz-v5/proxy/beacon/BeaconProxy.sol';
+
+import { UpgradeableBeacon } from '@solady/utils/UpgradeableBeacon.sol';
+
+import { OwnableUpgradeable } from '@ozu-v5/access/OwnableUpgradeable.sol';
+
+import { BeaconBase } from '../../lib/proxy/BeaconBase.sol';
+import { MatrixStrategyExecutor } from './MatrixStrategyExecutor.sol';
+
+contract MatrixStrategyExecutorFactory is BeaconBase, OwnableUpgradeable {
+  constructor() {
+    _disableInitializers();
+  }
+
+  function initialize(address owner_, address initialImpl) external initializer {
+    __Ownable_init(owner_);
+    __BeaconBase_init(new UpgradeableBeacon(address(this), address(initialImpl)));
+  }
+
+  function create(address owner_, address emergencyManager_) external onlyOwner returns (address) {
+    bytes memory args = abi.encodeCall(MatrixStrategyExecutor.initialize, (owner_, emergencyManager_));
+    address instance = address(new BeaconProxy(address(beacon()), args));
+
+    _pushInstance(instance);
+
+    return instance;
+  }
+
+  function callBeacon(bytes calldata data) external onlyOwner returns (bytes memory) {
+    return _callBeacon(data);
+  }
+}
