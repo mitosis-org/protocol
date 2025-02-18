fbf8468c50b8bb4a4fed627aae2689237fef246d
Seungbae Yu | Ray <dbadoy4874@gmail.com>
2025-02-18 17:03:31 +0900

    apply UUPS to GovernanceExecutorEntrypoint

--- src/branch/GovernanceExecutorEntrypoint.sol
+++ src/branch/GovernanceExecutorEntrypoint.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import { Ownable2StepUpgradeable } from '@ozu-v5/access/Ownable2StepUpgradeable.sol';
-import { OwnableUpgradeable } from '@ozu-v5/access/OwnableUpgradeable.sol';
+import { AccessControlUpgradeable } from '@ozu-v5/access/AccessControlUpgradeable.sol';
+import { UUPSUpgradeable } from '@ozu-v5/proxy/utils/UUPSUpgradeable.sol';
 
 import { GasRouter } from '@hpl-v5/client/GasRouter.sol';
 
@@ -12,7 +12,12 @@ import { StdError } from '../lib/StdError.sol';
 import '../message/Message.sol';
 import { GovernanceExecutor } from './GovernanceExecutor.sol';
 
-contract GovernanceExecutorEntrypoint is IGovernanceExecutorEntrypoint, GasRouter, Ownable2StepUpgradeable {
+contract GovernanceExecutorEntrypoint is
+  IGovernanceExecutorEntrypoint,
+  GasRouter,
+  UUPSUpgradeable,
+  AccessControlUpgradeable
+{
   using Message for *;
   using Conv for *;
 
@@ -31,8 +36,9 @@ contract GovernanceExecutorEntrypoint is IGovernanceExecutorEntrypoint, GasRoute
 
   function initialize(address owner_, address hook, address ism) public initializer {
     _MailboxClient_initialize(hook, ism, owner_);
-    __Ownable2Step_init();
-    _transferOwnership(owner_);
+    __UUPSUpgradeable_init();
+
+    _grantRole(DEFAULT_ADMIN_ROLE, owner_);
 
     _enrollRemoteRouter(_mitosisDomain, _mitosisAddr);
   }
@@ -61,13 +67,7 @@ contract GovernanceExecutorEntrypoint is IGovernanceExecutorEntrypoint, GasRoute
     }
   }
 
-  //=========== NOTE: OwnableUpgradeable & Ownable2StepUpgradeable
+  //=========== NOTE: Internal methods ===========//
 
-  function transferOwnership(address owner) public override(Ownable2StepUpgradeable, OwnableUpgradeable) {
-    Ownable2StepUpgradeable.transferOwnership(owner);
-  }
-
-  function _transferOwnership(address owner) internal override(Ownable2StepUpgradeable, OwnableUpgradeable) {
-    Ownable2StepUpgradeable._transferOwnership(owner);
-  }
+  function _authorizeUpgrade(address newImplementation) internal override onlyRole(DEFAULT_ADMIN_ROLE) { }
 }
