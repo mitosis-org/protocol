6f9c0001cd1a62edce06915d2cf0de5bf00a994e
Taeguk Kwon <xornrbboy@gmail.com>
2025-02-27 00:36:50 +0900

    generate bindings for use in consensus client

---
+++ tools/gen-bindings-for-consensus-layer.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+gen() {
+  target=$1
+
+  # e.g., ConsensusGovernanceEntrypoint
+  name=$(echo ${target} | cut -d ":" -f 2)
+  
+  # e.g., consensus_governance_entrypoint
+  file_name=$(echo ${name} | sed 's/\([A-Z]\)/_\1/g' | sed 's/^_//' | tr '[:upper:]' '[:lower:]')
+
+  tmp=$(mktemp -d)
+  abifile=${tmp}/${name}.abi
+  binfile=${tmp}/${name}.bin
+
+  forge inspect ${target} abi > ${abifile}
+  forge inspect ${target} bytecode > ${binfile}
+
+  abigen \
+    --abi ${abifile} \
+    --bin ${binfile} \
+    --type ${name} \
+    --pkg bindings \
+    --out ./bindings/${file_name}.go
+}
+
+TARGETS=(
+    "src/hub/consensus-layer/ConsensusGovernanceEntrypoint.sol:ConsensusGovernanceEntrypoint"
+    "src/hub/consensus-layer/ConsensusValidatorEntrypoint.sol:ConsensusValidatorEntrypoint"
+)
+
+rm -rf ./bindings && mkdir -p ./bindings
+
+for target in "${TARGETS[@]}"; do
+  gen ${target}
+done
