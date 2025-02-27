03c1b9f5e44bd2172d5ab6d04e452c38cfd085a1
taeguk | thai <xornrbboy@gmail.com>
2025-02-27 23:05:30 +0900

    👷‍♂️ Add scripts for deploying consensus entrypoints in testing (#256)

---
+++ script/DeployConsensusEntrypoints.s.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import { console } from '@std/console.sol';
+import { Script } from '@std/Script.sol';
+
+import { ERC1967Proxy } from '@oz-v5/proxy/ERC1967/ERC1967Proxy.sol';
+
+import { ConsensusGovernanceEntrypoint } from '../src/hub/consensus-layer/ConsensusGovernanceEntrypoint.sol';
+import { ConsensusValidatorEntrypoint } from '../src/hub/consensus-layer/ConsensusValidatorEntrypoint.sol';
+
+// NOTE: This is not for production use. This is just for testing and development purposes.
+contract DeployConsensusEntrypoints is Script {
+  function run() external {
+    vm.startBroadcast();
+
+    address owner = msg.sender;
+
+    ConsensusValidatorEntrypoint val = ConsensusValidatorEntrypoint(
+      payable(
+        new ERC1967Proxy(
+          address(new ConsensusValidatorEntrypoint()), abi.encodeCall(ConsensusValidatorEntrypoint.initialize, (owner))
+        )
+      )
+    );
+    ConsensusGovernanceEntrypoint gov = ConsensusGovernanceEntrypoint(
+      payable(
+        new ERC1967Proxy(
+          address(new ConsensusGovernanceEntrypoint()),
+          abi.encodeCall(ConsensusGovernanceEntrypoint.initialize, (owner))
+        )
+      )
+    );
+
+    val.setPermittedCaller(owner, true);
+    gov.setPermittedCaller(owner, true);
+
+    console.log('====================== Result ======================');
+    console.log('Owner                        : ', address(owner));
+    console.log('Permitted Caller             : ', address(owner));
+    console.log('ConsensusValidatorEntrypoint : ', address(val));
+    console.log('ConsensusGovernanceEntrypoint: ', address(gov));
+    console.log('====================================================');
+
+    vm.stopBroadcast();
+  }
+}
