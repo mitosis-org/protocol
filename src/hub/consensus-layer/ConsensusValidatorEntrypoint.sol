d29f4eaa1429b67dd3261fca24b1e6530d824480
Taeguk Kwon <xornrbboy@gmail.com>
2025-03-19 00:37:33 +0900

    fix: missing validator check

--- src/hub/consensus-layer/ConsensusValidatorEntrypoint.sol
+++ src/hub/consensus-layer/ConsensusValidatorEntrypoint.sol
@@ -119,6 +119,8 @@ contract ConsensusValidatorEntrypoint is IConsensusValidatorEntrypoint, Ownable2
   }
 
   function updateExtraVotingPower(address valAddr, uint256 extraVotingPower) external onlyPermittedCaller {
+    require(extraVotingPower % 1 gwei == 0, StdError.InvalidParameter('extraVotingPower'));
+
     emit MsgUpdateExtraVotingPower(valAddr, extraVotingPower / 1 gwei);
   }
 
