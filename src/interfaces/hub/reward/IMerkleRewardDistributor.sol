843b41ba2bc03abafcee66f493d2bd2b21717e1c
byeongsu-hong <hong@byeongsu.dev>
2025-03-25 17:03:56 +0900

    index treasury eventb

--- src/interfaces/hub/reward/IMerkleRewardDistributor.sol
+++ src/interfaces/hub/reward/IMerkleRewardDistributor.sol
@@ -16,7 +16,7 @@ interface IMerkleRewardDistributor {
     address indexed receiver, uint256 indexed stage, address indexed matrixVault, address[] rewards, uint256[] amounts
   );
 
-  event TreasuryUpdated(ITreasury oldTreasury, ITreasury newTreasury);
+  event TreasuryUpdated(address indexed oldTreasury, address indexed newTreasury);
 
   /**
    * @notice Error thrown when attempting to claim an already claimed reward.
