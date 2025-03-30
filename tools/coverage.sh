c1f45fa4fd2e99244233ae0c46270092f6d69d41
byeongsu-hong <hong@byeongsu.dev>
2025-03-31 05:08:50 +0900

    test: define base dir

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,24 +1,27 @@
 set -e # exit on error
 
+rm -rf coverage
+mkdir -p coverage
+
 # generates lcov.info
-forge coverage --report lcov -vvv
+forge coverage --report lcov --report-file coverage/lcov.info -vvv
 
 # Filter out node_modules, test, and mock files
 lcov \
     --rc derive_function_end_line=0 \
-    --remove lcov.info \
-    --output-file filtered-lcov.info \
+    --remove coverage/lcov.info \
+    --output-file coverage/filtered-lcov.info \
     --ignore-errors unused \
     --ignore-errors inconsistent \
     "*dependencies*" "*test*" "*script*" "*src/external*"
 
-rm lcov.info
-mv filtered-lcov.info lcov.info
+rm coverage/lcov.info
+mv coverage/filtered-lcov.info coverage/lcov.info
 
 # Generate summary
 lcov \
     --rc derive_function_end_line=0 \
-    --list lcov.info
+    --list coverage/lcov.info
 
 # Open more granular breakdown in browser
 if [ "$CI" != "true" ]; then
@@ -27,6 +30,6 @@ if [ "$CI" != "true" ]; then
         --output-directory coverage \
         --ignore-errors unused \
         --ignore-errors inconsistent \
-        lcov.info
+        coverage/lcov.info
     open coverage/index.html
 fi
