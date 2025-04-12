7be2b3ba7b5285d96a299f33f5df9ab6a5bc858d
ByeongSu Hong <hong@byeongsu.dev>
2025-04-13 01:07:43 +0900

    🐞 Prevent underflow (#340)
    fix

--- src/interfaces/hub/validator/IValidatorStaking.sol
+++ src/interfaces/hub/validator/IValidatorStaking.sol
@@ -25,6 +25,7 @@ interface IValidatorStaking {
   error IValidatorStaking__RedelegateToSameValidator(address valAddr);
   error IValidatorStaking__CooldownNotPassed(uint48 lastTime, uint48 currentTime, uint48 requiredCooldown);
   error IValidatorStaking__InsufficientMinimumAmount(uint256 minAmount);
+  error IValidatorStaking__InsufficientStakedAmount(uint256 requested, uint256 available);
 
   // ========== VIEWS ========== //
 
