8c33dc1abd79fa393d675a5fafe76f50c2b443d2
byeongsu-hong <hong@byeongsu.dev>
2025-04-24 18:29:52 +0900

    fix: happy

--- src/branch/MitosisVaultEntrypoint.sol
+++ src/branch/MitosisVaultEntrypoint.sol
@@ -43,9 +43,7 @@ contract MitosisVaultEntrypoint is
   }
 
   function initialize(address owner_, address hook, address ism) public initializer {
-    _MailboxClient_initialize(hook, ism, _msgSender());
-    _transferOwnership(owner_);
-
+    _MailboxClient_initialize(hook, ism, owner_);
     __Ownable2Step_init();
     __UUPSUpgradeable_init();
 
