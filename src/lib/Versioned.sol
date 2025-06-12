110d671193bfea7d1a916a361cb4eb92871b9f55
byeongsu-hong <hong@byeongsu.dev>
2025-06-12 22:05:16 +0900

    versioned

--- src/lib/Versioned.sol
+++ src/lib/Versioned.sol
@@ -5,5 +5,5 @@ pragma solidity ^0.8.28;
 contract Versioned {
   string public constant VERSION = 'v0.2.0-alpha';
 
-  address public constant COMMIT = 0xa2FC9620a8B97690eaf5fdDB1Dc56875A68C4b52;
+  address public constant COMMIT = 0x610046638589e85Cc3a79633130B29E5C4be7B59;
 }
