6a4d1be2f7d62a4b6403df1254cdcbf1a595f9c6
byeongsu-hong <hong@byeongsu.dev>
2025-06-13 16:20:50 +0900

    yes

--- src/lib/Versioned.sol
+++ src/lib/Versioned.sol
@@ -2,8 +2,10 @@
 // THIS IS GENERATED FILE. DO NOT EDIT.
 pragma solidity ^0.8.28;
 
-contract Versioned {
-  string public constant VERSION = 'v0.2.0-alpha';
+import { IVersioned } from '../interfaces/lib/IVersioned.sol';
 
-  address public constant COMMIT = 0x66c509685842BeC8aB3a6c3440309c5739F121e8;
+contract Versioned is IVersioned {
+  string public constant GIT_TAG = '{{git_tag}}';
+
+  string public constant GIT_COMMIT = '{{git_commit}}';
 }
