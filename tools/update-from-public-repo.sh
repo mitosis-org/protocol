e536a30f3f3d982c7cb2220162f780c65d407aff
thai <xornrbboy@gmail.com>
2025-07-01 19:35:07 +0900

    🥢 Remove scripts not used anymore (#386)

--- tools/update-from-public-repo.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for FILE in $(cat ./public-files.txt)
-do
-    echo "Copying ${FILE}"
-    mkdir -p $(dirname "${FILE}") && cp -rf "../protocol-public/${FILE}" "${FILE}"
-done
