7c6aebb99a364df662024c6f9a3c1a4fffc84536
byeongsu-hong <hong@byeongsu.dev>
2025-08-03 01:54:45 +0900

    reflect

--- src/branch/strategy/manager/DecodersAndSanitizers/ManagerDecoderAndSanitizer.sol
+++ src/branch/strategy/manager/DecodersAndSanitizers/ManagerDecoderAndSanitizer.sol
@@ -4,11 +4,9 @@ pragma solidity ^0.8.28;
 import { BaseDecoderAndSanitizer } from './BaseDecoderAndSanitizer.sol';
 
 contract ManagerDecoderAndSanitizer is BaseDecoderAndSanitizer {
-  function deposit(address asset, uint256) external pure returns (bytes memory addressesFound) {
-    addressesFound = abi.encodePacked(asset);
-  }
+  function deposit(uint256) external pure returns (bytes memory addressesFound) { }
 
-  function withdraw(address asset, uint256, address receiver) external pure returns (bytes memory addressesFound) {
-    addressesFound = abi.encodePacked(asset, receiver);
+  function withdraw(uint256, address receiver) external pure returns (bytes memory addressesFound) {
+    addressesFound = abi.encodePacked(receiver);
   }
 }
--- src/branch/strategy/tally/ManagerTally.sol
+++ src/branch/strategy/tally/ManagerTally.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.28;
 import { StdTally } from './StdTally.sol';
 
 interface IManager {
+  function asset() external view returns (address);
+  function deposit(uint256 amount) external;
+  function withdraw(uint256 amount, address receiver) external;
   function totalBalance() external view returns (uint256);
 }
 
