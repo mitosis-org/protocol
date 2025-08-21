fbb70937935c9850ad5a5ec2cbbd2092fc7035b8
Eddy <hong@byeongsu.dev>
2025-08-21 20:02:00 +0700

    🥢 Ready to publish on Soldeer (#429)
    * publish
    
    * script
    
    * bump
    
    * var
    
    * remove clean
    
    * Revert "remove clean"
    
    This reverts commit b3d8683b9cf873e85ad3d5f7988cc159c0c61495.

---
+++ tools/publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+gitTag=$(git describe --abbrev=0 --tags origin/main)
+gitCommit=$(git rev-parse origin/main)
+
+mkdir -p temp 
+cp -r src temp 
+cp foundry.toml temp/src
+
+sed \
+  -e "s/{{git_tag}}/$gitTag/g" \
+  -e "s/{{git_commit}}/$gitCommit/g" \
+  temp/src/lib/Versioned.sol > temp/src/lib/Versioned.sol.tmp
+
+mv temp/src/lib/Versioned.sol.tmp temp/src/lib/Versioned.sol
+
+# ignore errors and continue to rm -rf temp
+forge soldeer push mitosis~$gitTag temp/src || true
+
+rm -rf temp
