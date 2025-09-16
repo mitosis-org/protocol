20eae35291aa19a573f780cae0526271fe4b2a7f
Seungbae Yu | Ray <dbadoy4874@gmail.com>
2025-09-16 17:55:12 +0900

    Update MitosisVaultEntrypoint.sol

--- src/branch/MitosisVaultEntrypoint.sol
+++ src/branch/MitosisVaultEntrypoint.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.28;
 
-import { StandardHookMetadata } from '@hpl/hooks/libs/StandardHookMetadata.sol';
 import { IMessageRecipient } from '@hpl/interfaces/IMessageRecipient.sol';
 
 import { Ownable2StepUpgradeable } from '@ozu/access/Ownable2StepUpgradeable.sol';
